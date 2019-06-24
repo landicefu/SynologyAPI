824a1586047635975d0819cc1acd0a04953c585e
Landice Fu <landice.fu@honestbee.com>
2019-06-24 14:53:59 +0800

    update module settings

--- src/main/kotlin/tw/lifehackers/synology/SynologyNAS.kt
+++ src/main/kotlin/tw/lifehackers/synology/SynologyNAS.kt
@@ -6,7 +6,6 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import tw.lifehackers.synology.exceptions.UnauthorizedException
 import tw.lifehackers.synology.api.SynologyApi
 import tw.lifehackers.synology.api.UnauthenticatedApi
